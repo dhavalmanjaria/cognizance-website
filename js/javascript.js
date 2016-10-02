651a522793478b1a4586d4170981c22832fdf5cd
dhavalmanjaria <dhavalmanjaria@gmail.com>
2016-10-02 23:38:41 +0530

    Added some JS proof of concept code but it is too late to write the rest of that

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,6 @@
-<
+$(document).ready(function(){
+	
+	$('#cs-title-text').text("Gooseman-Cliffe");
+	$('#cs-content-text').text("A long standing tradition of tech fests everywhere. This event always draws a crowd. from the biggest Universities to the smallest colleges, gamers from all over are drawn to this event to show off their skills. ");
+	
+});
