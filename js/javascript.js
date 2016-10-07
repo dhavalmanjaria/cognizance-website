d8f750056fb7ed761e60870f7bf49df2412e37bb
Dhaval Anjaria <dhv2712@gmail.com>
2016-10-07 06:07:53 +0530

    Fixed the card size issue and footer scroll bar issue

--- js/javascript.js
+++ js/javascript.js
@@ -3,12 +3,13 @@ $(document).ready(function(){
 	$('#cs-title-text').text("Gooseman-Cliffe");
 	$('#cs-content-text').text("A long standing tradition of tech fests everywhere. This event always draws a crowd. from the biggest Universities to the smallest colleges, gamers from all over are drawn to this event to show off their skills. ");
 	
-	var footerHTML = "<div class='mdl-mini-footer__center-section'>";
-	footerHTML += "<div class='mdl-logo'>Cognizance</div>"
-	footerHTML += "<div class='mdl-logo'><a href='http://ibsar.ac.in'>IBSAR Main Page</a></div>";
-	footerHTML += "<div class='mdl-logo'><a href='contact.html'>Contact</a></div>";
-	footerHTML += "<div class='mdl-logo'>Location: Thapar Complex, Sector 15, CBD Belapur, Navi Mumbai</div>";
-	footerHTML += "</div>";
+	var footerHTML = "<div class='mdl-mini-footer__left-section'>";
+	footerHTML += "<ul class='mdl-mini-footer__link-list'>";
+	footerHTML += "<li><div class='mdl-logo'>Cognizance</div></li>";
+	footerHTML += "<li><div><a href='http://ibsar.ac.in'>IBSAR Main Page</a></div></li>";
+	footerHTML += "<li><div><a href='contact.html'>Contact</a></div></li>";
+	footerHTML += "<li><div>Location: Thapar Complex, Sector 15, CBD Belapur, Navi Mumbai</div></li>";
+	footerHTML += "</ul></div>";
 	
 	$('footer').addClass('')
 	$('footer').html(footerHTML);
